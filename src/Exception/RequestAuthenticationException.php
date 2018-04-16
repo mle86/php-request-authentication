1fd99d8473afba37e3816e7e32709d2b63716bc4
mle <maximilian@eul.cc>
2018-04-16 12:18:11 +0200

    init

---
+++ src/Exception/RequestAuthenticationException.php
@@ -0,0 +1,10 @@
+<?php
+namespace mle86\RequestAuthentication\Exception;
+
+/**
+ * Common interface for all exceptions thrown by this library.
+ */
+interface RequestAuthenticationException
+{
+
+}
