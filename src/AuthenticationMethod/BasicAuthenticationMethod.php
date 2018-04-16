f7846cf182764d1915924b286866e4173b040edd
mle <maximilian@eul.cc>
2018-04-16 22:45:26 +0200

    + BasicAuthenticationMethod class and test

---
+++ src/AuthenticationMethod/BasicAuthenticationMethod.php
@@ -0,0 +1,58 @@
+<?php
+namespace mle86\RequestAuthentication\AuthenticationMethod;
+
+use mle86\RequestAuthentication\DTO\RequestInfo;
+use mle86\RequestAuthentication\Exception\CryptoErrorException;
+use mle86\RequestAuthentication\Exception\InvalidAuthenticationException;
+use mle86\RequestAuthentication\KeyRepository\KeyRepository;
+
+/**
+ * Implements HTTP “Basic” authentication.
+ *
+ * The Basic username is the $api_client_id,
+ * the Basic password is the $api_secret_key.
+ * The Basic password must also be returned unchanged by the KeyRepository.
+ *
+ * @internal
+ *   This is here mostly for testing and completeness purposes;
+ *   whatever HTTP/REST/PSR-7 library you're using,
+ *   it can probably do this better.
+ */
+class BasicAuthenticationMethod
+    implements AuthenticationMethod
+{
+
+    const HEADER = 'Authorization';
+
+    public function authenticate(RequestInfo $request, string $api_client_id, string $api_secret_key): array
+    {
+        return [self::HEADER => 'Basic ' . base64_encode($api_client_id . ':' . $api_secret_key)];
+    }
+
+    public function verify(RequestInfo $request, KeyRepository $keys): void
+    {
+        $header = $request->getNonemptyHeaderValue(self::HEADER);
+
+        if (strtolower(substr($header, 0,6)) !== 'basic ') {
+            throw new InvalidAuthenticationException('invalid ' . self::HEADER . ' header');
+        }
+        $header = trim(substr($header, 6));  // remove 'Basic ' prefix
+
+        $decoded = base64_decode($header, true);
+        if ($decoded === false || $decoded === null) {
+            throw new CryptoErrorException('base64_decode failed');
+        }
+
+        if (strpos($decoded, ':') === false) {
+            throw new InvalidAuthenticationException('invalid ' . self::HEADER . ' header payload');
+        }
+        [$username, $password] = explode(':', $decoded, 2);
+
+        $known_password = $keys[$username];
+
+        if (!hash_equals($known_password, $password)) {
+            throw new InvalidAuthenticationException('auth password mismatch');
+        }
+    }
+
+}
---
+++ test/BasicAuthenticationMethodTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace mle86\RequestAuthentication\Tests;
+
+use GuzzleHttp\Psr7\Request;
+use mle86\RequestAuthentication\AuthenticationMethod\AuthenticationMethod;
+use mle86\RequestAuthentication\AuthenticationMethod\BasicAuthenticationMethod;
+use mle86\RequestAuthentication\DTO\RequestInfo;
+use mle86\RequestAuthentication\Tests\Helper\AuthenticationMethodTests;
+use PHPUnit\Framework\TestCase;
+
+class BasicAuthenticationMethodTest
+    extends TestCase
+{
+    use AuthenticationMethodTests;
+
+    protected function methodOutputDependsOnRequestData(): bool
+    {
+        // It's just a header containing the username and the password, it doesn't change if the input changes.
+        return false;
+    }
+
+    protected function customKeyRepositoryEntries(): array { return [
+        'U0000001' => 'topSecret:2018',
+        // Needed for checkKnownValues().
+        // base64_encode("U0000001:topSecret:2018") === "VTAwMDAwMDE6dG9wU2VjcmV0OjIwMTg="
+    ]; }
+
+
+    public function testGetInstance(): AuthenticationMethod
+    {
+        return new BasicAuthenticationMethod;
+    }
+
+    protected function otherTests(AuthenticationMethod $method): void
+    {
+        $this->checkKnownValues($method);
+    }
+
+    protected function checkKnownValues(AuthenticationMethod $method)
+    {
+        $request = new Request(
+            'PUT', 'http://basic.test.localhost/foo',
+            ['Content-Type: application/json; encoding=utf-8'],
+            'SomeBody');
+
+        $ri = RequestInfo::fromPsr7($request);
+
+        [$username, $password] = [
+            array_keys  ($this->customKeyRepositoryEntries())[0],
+            array_values($this->customKeyRepositoryEntries())[0],
+        ];
+
+        $headers = $method->authenticate($ri, $username, $password);
+
+        // Should add an 'Authorization' header and nothing else:
+        $this->assertSame(['Authorization'], array_keys($headers));
+
+        // The header should contain the correct base64 string:
+        $regex = '/^Basic +' . preg_quote(base64_encode($username . ':' . $password), '/') . '\s*$/';
+        $this->assertRegExp($regex, $headers['Authorization']);
+
+        // And of course, the header should be accepted by BasicAuthenticationMethod itself:
+        $this->checkValidResult($request, $headers, $method);
+    }
+
+}
